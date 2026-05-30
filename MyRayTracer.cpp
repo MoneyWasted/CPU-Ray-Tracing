9f9027e8baad86c20cecc1e9b7d6f41019cb28c2
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2026-05-30 14:34:56 -0400

    Delete MyRayTracer.cpp

--- MyRayTracer.cpp
+++
@@ -1,1 +0,0 @@
-	Quads[2] = CQuad(R * vec3(0.5f, -2.0f, 0.5f) + V, R * vec3(0.5f, -2.0f, -0.5f) + V, R * vec3(0.5f, -1.0f, -0.5f) + V, R * vec3(0.5f, -1.0f, 0.5f) + V, vec3(1.0f, 1.0f, 1.0f), &Cube);
